bebe9bc6a5240d9b3d6fc74f6b6b0fcb8039f6e2
Linying Zhang <lz2629@dbmi.columbia.edu>
2020-11-06 18:25:39 -0500

    add sql

--- extras/CodeToRun.r
+++ extras/CodeToRun.r
@@ -16,7 +16,7 @@
 
 # Install devtools from CRAN
 # install.packages("devtools")
-# devtools::install_github("ohdsi/SqlRender")
+devtools::install_github("ohdsi/SqlRender")
 # devtools::install_github("ohdsi/DatabaseConnector")
 # devtools::install_github("ohdsi/FeatureExtraction")
 # devtools::install_github("ohdsi/PatientLevelPrediction")
@@ -57,13 +57,19 @@ targetCohortId <- 1
 
 
 
-# Create study cohort table structure:
+# Create drug exposure table and measurement table:
 drugExposureTable <- "potassium_cohort_drug_exposure"
-sql <- SqlRender::loadRenderTranslateSql(sqlFilename = "extractDrug.sql",
+measurementTable <- "POTASSIUM_COHORT_MEASUREMENT"
+sql <- SqlRender::loadRenderTranslateSql(sqlFilename = "extractDrugAndMeas.sql",
                                          packageName = "MvDeconfounder",
                                          dbms = attr(connection, "dbms"),
                                          oracleTempSchema = NULL,
                                          drug_exposure_table = drugExposureTable,
+                                         measurement_table = measurementTable,
+                                         target_cohort_id = targetCohortId,
+                                         measurement_concept_id = measurementConceptId,
+                                         observation_window_before = observationWindowBefore,
+                                         observation_window_after = observationWindowAfter,
                                          cdm_database_schema = cdmDatabaseSchema,
                                          target_cohort_id = targetCohortId,
                                          target_cohort_table = targetCohortTable,
@@ -71,83 +77,78 @@ sql <- SqlRender::loadRenderTranslateSql(sqlFilename = "extractDrug.sql",
 
 DatabaseConnector::executeSql(connection, sql, progressBar = TRUE, reportOverallTime = FALSE)
 
-# Create study cohort table structure:
-measExposureTable <- "potassium_cohort_drug_exposure"
-sql <- SqlRender::loadRenderTranslateSql(sqlFilename = "extractDrug.sql",
-                                         packageName = "MvDeconfounder",
-                                         dbms = attr(connection, "dbms"),
-                                         oracleTempSchema = NULL,
-                                         drug_exposure_table = drugExposureTable,
-                                         cdm_database_schema = cdmDatabaseSchema,
-                                         target_cohort_id = targetCohortId,
-                                         target_cohort_table = targetCohortTable,
-                                         target_database_schema = cohortDatabaseSchema)
-
-DatabaseConnector::executeSql(connection, sql, progressBar = TRUE, reportOverallTime = FALSE)
-
-
-
-## Multivariate deconfounder
-targetCohortTable <- 'MVDECONFOUNDER_COHORT'
-targetCohortId <- 1
-DataFolder <- 'dat/PackageTest'
-ResFolder <- 'res/PackageTest'
-ingredientList <- readRDS(file = file.path(DataFolder, 'ingredientList.rds'))
-ingredientConceptIds <-ingredientList[[1]]$conceptId
-measurementList <- readRDS(file = file.path(DataFolder,'measurementList.rds'))
-measurementConceptIds <- measurementList[[1]]$conceptId
-mVdData <- MvDeconfounder::generateMvdData(connection = connection,
-                                           cdmDatabaseSchema = cdmDatabaseSchema,
-                                           oracleTempSchema = NULL,
-                                           vocabularyDatabaseSchema = cdmDatabaseSchema,
-                                           cohortDatabaseSchema =cohortDatabaseSchema,
-                                           targetCohortTable = targetCohortTable,
-                                           minimumProportion = 0.001,
-                                           targetDrugTable = 'DRUG_ERA',
-                                           ingredientConceptIds = ingredientConceptIds,
-                                           measurementConceptIds = measurementConceptIds,
-                                           createTargetCohort = F,
-                                           extractDrugFeature = T,
-                                           extractMeasFeature = T,
-                                           labWindow = 35,
-                                           targetCohortId = targetCohortId,
-                                           temporalStartDays = c(-35, 1),
-                                           temporalEndDays =c(-1, 35),
-                                           sampleSize = NULL,
-                                           DataFolder)
-
-result <- MvDeconfounder::unadjIndeptRidgeReg(DataFolder,
-                                              ResFolder,
-                                              cvFold = 5,
-                                              lambda = 10^seq(2, -2, by = -0.1))
-plpData.meas <-PatientLevelPrediction::loadPlpData(file = 'dat/20191116Complete/plpData.meas')
-plpData.drug <-PatientLevelPrediction::loadPlpData(file = 'dat/20191116Complete/plpData.drug')
-
-# specify the python to use, i.e. from a conda env
-reticulate::use_condaenv("deconfounder_py3", required = TRUE)
-
-learning_rate <- 0.01
-max_steps <- as.integer(5000)
-latent_dim <- as.integer(1)
-batch_size <- as.integer(1024)
-num_samples <- as.integer(1)
-holdout_portion <- 0.2
-print_steps <- as.integer(50)
-tolerance <- as.integer(3)
-num_confounder_samples <- as.integer(100)
-CV <- as.integer(5)
-outcome_type <- "linear"
-project_dir <- "C:/Users/lz2629/git/zhangly811/MvDeconfounder"
-
-MvDeconfounder::fitDeconfounder(learning_rate,
-                                max_steps,
-                                latent_dim,
-                                batch_size,
-                                num_samples,
-                                holdout_portion,
-                                print_steps,
-                                tolerance,
-                                num_confounder_samples,
-                                CV,
-                                outcome_type,
-                                project_dir)
+# load data into R
+sql<-SqlRender::render("SELECT * FROM @target_database_schema.@measurement_table",
+                          target_database_schema = cohortDatabaseSchema,
+                          measurement_table = measurementTable)
+meas <- DatabaseConnector::querySql(connection, sql)
+sql<-SqlRender::render("SELECT * FROM @target_database_schema.@drug_exposure_table",
+                       target_database_schema = cohortDatabaseSchema,
+                       drug_exposure_table = drugExposureTable)
+drug <- DatabaseConnector::querySql(connection, sql)
+#
+#
+# ## Multivariate deconfounder
+# targetCohortTable <- 'MVDECONFOUNDER_COHORT'
+# targetCohortId <- 1
+# DataFolder <- 'dat/PackageTest'
+# ResFolder <- 'res/PackageTest'
+# ingredientList <- readRDS(file = file.path(DataFolder, 'ingredientList.rds'))
+# ingredientConceptIds <-ingredientList[[1]]$conceptId
+# measurementList <- readRDS(file = file.path(DataFolder,'measurementList.rds'))
+# measurementConceptIds <- measurementList[[1]]$conceptId
+# mVdData <- MvDeconfounder::generateMvdData(connection = connection,
+#                                            cdmDatabaseSchema = cdmDatabaseSchema,
+#                                            oracleTempSchema = NULL,
+#                                            vocabularyDatabaseSchema = cdmDatabaseSchema,
+#                                            cohortDatabaseSchema = cohortDatabaseSchema,
+#                                            targetCohortTable = targetCohortTable,
+#                                            minimumProportion = 0.001,
+#                                            targetDrugTable = 'DRUG_ERA',
+#                                            ingredientConceptIds = ingredientConceptIds,
+#                                            measurementConceptIds = measurementConceptIds,
+#                                            createTargetCohort = F,
+#                                            extractDrugFeature = T,
+#                                            extractMeasFeature = T,
+#                                            labWindow = 35,
+#                                            targetCohortId = targetCohortId,
+#                                            temporalStartDays = c(-35, 1),
+#                                            temporalEndDays =c(-1, 35),
+#                                            sampleSize = NULL,
+#                                            DataFolder)
+#
+# result <- MvDeconfounder::unadjIndeptRidgeReg(DataFolder,
+#                                               ResFolder,
+#                                               cvFold = 5,
+#                                               lambda = 10^seq(2, -2, by = -0.1))
+# plpData.meas <-PatientLevelPrediction::loadPlpData(file = 'dat/20191116Complete/plpData.meas')
+# plpData.drug <-PatientLevelPrediction::loadPlpData(file = 'dat/20191116Complete/plpData.drug')
+#
+# # specify the python to use, i.e. from a conda env
+# reticulate::use_condaenv("deconfounder_py3", required = TRUE)
+#
+# learning_rate <- 0.01
+# max_steps <- as.integer(5000)
+# latent_dim <- as.integer(1)
+# batch_size <- as.integer(1024)
+# num_samples <- as.integer(1)
+# holdout_portion <- 0.2
+# print_steps <- as.integer(50)
+# tolerance <- as.integer(3)
+# num_confounder_samples <- as.integer(100)
+# CV <- as.integer(5)
+# outcome_type <- "linear"
+# project_dir <- "C:/Users/lz2629/git/zhangly811/MvDeconfounder"
+#
+# MvDeconfounder::fitDeconfounder(learning_rate,
+#                                 max_steps,
+#                                 latent_dim,
+#                                 batch_size,
+#                                 num_samples,
+#                                 holdout_portion,
+#                                 print_steps,
+#                                 tolerance,
+#                                 num_confounder_samples,
+#                                 CV,
+#                                 outcome_type,
+#                                 project_dir)
---
+++ inst/sql/sql_server/extractDrugAndMeas.sql
@@ -0,0 +1,47 @@
+IF OBJECT_ID('@target_database_schema.@drug_exposure_table', 'U') IS NOT NULL
+DROP TABLE @target_database_schema.@drug_exposure_table;
+IF OBJECT_ID('@target_database_schema.@measurement_table', 'U') IS NOT NULL
+DROP TABLE @target_database_schema.@measurement_table;
+
+CREATE TABLE @target_database_schema.@drug_exposure_table(
+    subject_id VARCHAR(255),
+    cohort_start_date DATE,
+    drug_exposure_start_date DATE,
+    drug_concept_id VARCHAR(255),
+    ancestor_concept_id VARCHAR(255),
+    concept_name VARCHAR(255)
+);
+
+INSERT INTO @target_database_schema.@drug_exposure_table(subject_id, cohort_start_date, drug_exposure_start_date, drug_concept_id, ancestor_concept_id, concept_name)
+
+SELECT DISTINCT SUBJECT_ID, cohort_start_date, drug_exposure_start_date, drug_concept_id, ancestor_concept_id, concept_name FROM @target_database_schema.@target_cohort_table
+JOIN @cdm_database_schema.drug_exposure ON @target_cohort_table.SUBJECT_ID = @cdm_database_schema.drug_exposure.person_id
+JOIN @cdm_database_schema.concept_ancestor ON drug_exposure.drug_concept_id = @cdm_database_schema.concept_ancestor.descendant_concept_id
+JOIN @cdm_database_schema.concept ON @cdm_database_schema.concept_ancestor.ancestor_concept_id=@cdm_database_schema.concept.concept_id
+WHERE COHORT_DEFINITION_ID = @target_cohort_id
+and @cdm_database_schema.drug_exposure.drug_exposure_start_date <= DATEADD(day, 7, CAST(@target_database_schema.@target_cohort_table.cohort_start_date AS DATE))
+and @cdm_database_schema.drug_exposure.drug_exposure_start_date >= @target_database_schema.@target_cohort_table.cohort_start_date
+and drug_concept_id != 0
+and concept_class_id = 'Ingredient';
+
+
+-- measurement extraction
+CREATE TABLE  @target_database_schema.@measurement_table(
+    person_id VARCHAR(255),
+    cohort_start_date DATE,
+    measurement_concept_id VARCHAR(255),
+    measurement_date DATE,
+    value_as_number FLOAT,
+    concept_name VARCHAR(255)
+);
+
+INSERT INTO @target_database_schema.@measurement_table(person_id, cohort_start_date, measurement_concept_id, measurement_date, value_as_number, concept_name)
+SELECT DISTINCT person_id, cohort_start_date, measurement_concept_id, measurement_date, value_as_number, concept_name FROM @target_database_schema.@target_cohort_table
+JOIN @cdm_database_schema.measurement ON @target_cohort_table.SUBJECT_ID = @cdm_database_schema.measurement.person_id
+JOIN @cdm_database_schema.concept ON @cdm_database_schema.measurement.measurement_concept_id = @cdm_database_schema.concept.concept_id
+WHERE COHORT_DEFINITION_ID = @target_cohort_id
+and measurement_concept_id = @measurement_concept_id
+and @cdm_database_schema.measurement.measurement_date > DATEADD(day, -@observation_window_before, CAST(@target_database_schema.@target_cohort_table.cohort_start_date AS DATE))
+and @cdm_database_schema.measurement.measurement_date <= DATEADD(day, @observation_window_after, CAST(@target_database_schema.@target_cohort_table.cohort_start_date AS DATE))
+and domain_id = 'Measurement'
+and standard_concept = 'S';
